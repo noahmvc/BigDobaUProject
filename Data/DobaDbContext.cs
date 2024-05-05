9b9dbfadfa7f0fced6ef54d4db7712b6f5f50c60
Noah Stacy <nas00001@mix.wvu.edu>
2024-05-05 15:12:22 -0400

    Add project files.

---
+++ Data/DobaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BigDobaUProject.Data
+{
+    public class DobaDbContext : DbContext
+    {
+        public DobaDbContext() { }
+        public DobaDbContext(DbContextOptions<DobaDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+        }
+    }
+}
